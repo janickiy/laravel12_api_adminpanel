77d0716325cd7bb735a64b176821849e8b6b9895
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2026-02-08 21:25:27 +0300

    add files to project

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Админ',
+            'login' => 'admin',
+            'role' => 'admin',
+            'password' => app('hash')->make('1234567'),
+        ]);
+    }
+}
