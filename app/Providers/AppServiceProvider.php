104e1a8e1c41abad15874a6a273c7bffc82e9b50
Яницкий Александр Анатольевич <a.yanitskiy@els24.com>
2026-02-08 22:07:59 +0300

    add files to project

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+
+use App\Helpers\PermissionsHelper;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('PermissionsHelper', PermissionsHelper::class);
     }
 
     /**
